7dcb7ad11c41d7c8fa3490449642595c8da8e29b
Oliver Wegner <void1976@gmail.com>
2018-11-15 13:56:27 +0100

    implementing complete oauth2 authorization flow

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,60 @@
 import * as express from "express";
+import { Application, Request, Response } from "express";
 import * as https from "https";
 import axios from "axios";
 import * as qs from "querystring";
+import * as path from "path";
 
-const app = express();
+async function run(app: Application, env) {
+    //app.use(express.static(__dirname + '/public'));
 
-const baseUrl = process.env.BASEURL || "http://localhost";
-const port = process.env.PORT || 8080;
-const clientId = process.env.API_CLIENTID;
-const clientSecret = process.env.API_CLIENTSECRET;
-const apiAuthUrl = process.env.API_AUTHURL;
-const apiTokenUrl = process.env.API_TOKENURL;
-const scopes = process.env.SCOPES || "openid";
+    app.get('/', async (req: Request, res: Response) => {
+        res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+    });
 
-function oauth1() {
-    //https.get();
-}
-
-function run() {
-    app.get('/', (req, res) => {
+    app.get('/oauth2', async (req: Request, res: Response) => {
         const querystring = qs.stringify({
-            client_id: clientId,
-            redirect_uri: `${baseUrl}:${port}/callback`,
+            client_id:     env.clientId,
+            redirect_uri:  `${env.baseUrl}:${env.port}/oauth2/callback`,
             response_type: "code",
-            scope: scopes,
+            scope:         env.scopes,
         });
 
-        res.redirect(`${apiAuthUrl}?${querystring}`);
+        const apiUrl = `${env.apiAuthUrl}?${querystring}`;
+
+        console.log(`Will redirect User agent to ${apiUrl}`);
+        res.redirect(302, apiUrl);
     });
 
-    app.get('/callback', (req, res) => {
-        console.log(req);
-        res.send('CALLBACK!');
+    app.get('/oauth2/callback', async (req: Request, res: Response) => {
+        const code = req.query.code;
+
+        const tokenRes = await axios.post(env.apiTokenUrl, qs.stringify({
+            grant_type:    "authorization_code",
+            client_id:     env.clientId,
+            client_secret: env.clientSecret,
+            redirect_uri:  `${env.baseUrl}:${env.port}/oauth2/callback`,
+            code,
+        })).catch((err) => {
+            console.log("ERROR:", err);
+        });
+        const tokenData = tokenRes && tokenRes.data;
+
+        tokenRes && res.header("Content-type", tokenRes.headers['content-type']);
+        res.send(tokenData);
     });
 
-    app.listen(port);
+    app.listen(env.port, () => console.log(`Listening on port ${env.port}.`));
 }
 
-run();
+const env = {
+    baseUrl:      process.env.BASEURL || "http://localhost",
+    port:         process.env.PORT || "8080",
+    clientId:     process.env.API_CLIENTID,
+    clientSecret: process.env.API_CLIENTSECRET,
+    apiAuthUrl:   process.env.API_AUTHURL,
+    apiTokenUrl:  process.env.API_TOKENURL,
+    scopes:       process.env.SCOPES || "openid",
+};
+
+run(express(), env);
